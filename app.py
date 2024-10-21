12cf2c45ff50cc018d18439d8e97d33cbe0b0ed0
vlarobbyk <sparthakus@gmail.com>
2024-10-21 03:38:36 -0500

    Corrected code added. Modified the index.html template (add an image header).

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 
+# Author: vlarobbyk
+# Version: 1.0
+# Date: 2024-10-20
+# Description: A simple example to process video captured by the ESP32-XIAO-S3 or ESP32-CAM-MB in Flask.
+
+
 from flask import Flask, render_template, Response, stream_with_context, Request
 from io import BytesIO
 
@@ -7,16 +13,16 @@ import numpy as np
 import requests
 
 app = Flask(__name__)
+# IP Address
 _URL = 'http://10.0.0.3'
+# Default Streaming Port
 _PORT = '81'
+# Default streaming route
 _ST = '/stream'
+SEP = ':'
 
-#video = cv2.VideoCapture(_URL+':'+_PORT+_ST)
-video = cv2.VideoCapture("http://10.0.0.3:81/stream")
-stream_url = "http://10.0.0.3:81/stream"
+stream_url = ''.join([_URL,SEP,_PORT,_ST])
 
-if not video.isOpened():
-    print('Not opened')
 
 def video_capture():
     res = requests.get(stream_url, stream=True)
@@ -27,7 +33,20 @@ def video_capture():
                 img_data = BytesIO(chunk)
                 cv_img = cv2.imdecode(np.frombuffer(img_data.read(), np.uint8), 1)
                 gray = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
-                (flag, encodedImage) = cv2.imencode(".jpg", gray)
+                N = 537
+                height, width = gray.shape
+                noise = np.full((height, width), 0, dtype=np.uint8)
+                random_positions = (np.random.randint(0, height, N), np.random.randint(0, width, N))
+                
+                noise[random_positions[0], random_positions[1]] = 255
+
+                noise_image = cv2.bitwise_or(gray, noise)
+
+                total_image = np.zeros((height, width * 2), dtype=np.uint8)
+                total_image[:, :width] = gray
+                total_image[:, width:] = noise_image
+
+                (flag, encodedImage) = cv2.imencode(".jpg", total_image)
                 if not flag:
                     continue
 
@@ -38,34 +57,16 @@ def video_capture():
                 print(e)
                 continue
 
-'''
-    while (3==3):
-        ret, frame = video.read()
-        gray = None
-
-        #print(f'Ret = {ret}')
-
-        if ret:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            (flag, encodedImage) = cv2.imencode(".jpg", gray)
-            if not flag:
-                continue
-
-            yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-			bytearray(encodedImage) + b'\r\n')
-'''
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/video_feed")
-def video_feed():
+@app.route("/video_stream")
+def video_stream():
     return Response(video_capture(),
                     mimetype="multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == "__main__":
     app.run(debug=False)
 
-video.release()
